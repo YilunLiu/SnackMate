b4677f5d87782cb94578778cb4c6d3215ce4d302
axlwu1994 <axlwu1994@gmail.com>
2015-10-25 00:44:23 -0700

    Added Spinner in toolbar and dropdown menu in HomePage.java.
    Having bug with displaying 5 spinner in the same row.

--- app/src/main/java/com/hexad/snackmate/HomePage.java
+++ app/src/main/java/com/hexad/snackmate/HomePage.java
@@ -15,7 +15,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class HomePage extends AppCompatActivity
@@ -25,6 +27,8 @@ public class HomePage extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
 
         setContentView(R.layout.activity_home_page);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -51,6 +55,32 @@ public class HomePage extends AppCompatActivity
         // Set up gridview on the homepage
         GridView gridView = (GridView) findViewById(R.id.homepage_gridview);
         gridView.setAdapter(new ImageAdapter(this));
+
+
+        Spinner menu = (Spinner) findViewById(R.id.spinner1);
+        String[] names = new String[]{"C", "Japan", "Korea", "China", "Others"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, names);
+        menu.setAdapter(adapter);
+
+        Spinner menu1 = (Spinner) findViewById(R.id.spinner2);
+        String[] names1 = new String[]{"T", "Spicy", "Salty","Sweet"};
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, names1);
+        menu1.setAdapter(adapter1);
+
+        Spinner menu2 = (Spinner) findViewById(R.id.spinner3);
+        String[] names2 = new String[]{"R", "5", "4", "3", "2", "1"};
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, names2);
+        menu2.setAdapter(adapter2);
+
+        Spinner menu3 = (Spinner) findViewById(R.id.spinner4);
+        String[] names3 = new String[]{"P", "from High to Low", "from Low to High"};
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, names3);
+        menu3.setAdapter(adapter3);
+
+        Spinner menu4 = (Spinner) findViewById(R.id.spinner5);
+        String[] names4 = new String[]{"O", "A-Z", "Z-A"};
+        ArrayAdapter<String> adapter4 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, names4);
+        menu4.setAdapter(adapter4);
 
     }
 
