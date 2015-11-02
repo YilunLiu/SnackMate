9354263a08cbcf16e00d009df1cc71f69833569e
Yilun Liu <yil171@ucsd.edu>
2015-11-01 21:25:16 -0800

    Add SnackItemService.getAllSnackItemsSync

--- app/src/main/java/com/hexad/snackmate/Items/SnackItemService.java
+++ app/src/main/java/com/hexad/snackmate/Items/SnackItemService.java
@@ -1,11 +1,29 @@
 package com.hexad.snackmate.Items;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
 import com.parse.ParseQuery;
+
+import java.util.List;
 
 /**
  * Created by yilunliu on 10/29/15.
  */
 public class SnackItemService {
 
-    
+    static public void getAllSnackItems(FindCallback<SnackItem> callback){
+        ParseQuery<SnackItem> query = SnackItem.getQuery();
+    }
+
+    static public List<SnackItem> getAllSnackItemsSync(){
+        ParseQuery<SnackItem> query = SnackItem.getQuery();
+        List<SnackItem> list = null;
+        try {
+            list = query.find();
+        }
+        catch (ParseException e){
+            //TODO
+        }
+        return list;
+    }
 }
