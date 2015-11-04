9ea76a5ffa0932c4196cdee7ae0212cb40648820
Michael <michaelmingjun@gmail.com>
2015-11-03 22:01:23 -0800

    Added missing SortType class to remote repo

---
+++ app/src/main/java/com/hexad/snackmate/Enumerations/SortType.java
@@ -0,0 +1,8 @@
+package com.hexad.snackmate.Enumerations;
+
+/**
+ * Created by Michael on 2015/11/1.
+ */
+public enum SortType {
+    Price_high_to_low, Price_low_to_high, Rating, Alphabetical, Default;
+}
