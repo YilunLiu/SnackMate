421724cd8989a1ad3dfc8524b07f2ae82e8370f1
axlwu1994 <axlwu1994@gmail.com>
2015-11-13 00:16:53 +0800

    adding wishlist new pages

--- app/src/main/java/com/hexad/snackmate/WishList.java
+++ app/src/main/java/com/hexad/snackmate/WishList.java
@@ -2,17 +2,25 @@ package com.hexad.snackmate;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
+import android.widget.ImageView;
 
 /**
  * Created by AxlWu on 2015/11/12.
  */
 public class WishList extends Activity {
 
+        private ImageView appImageView;
+        @Override
         protected void onCreate(Bundle SavedInstanceState ){
             super.onCreate(SavedInstanceState);
 
             requestWindowFeature(Window.FEATURE_NO_TITLE);
             setContentView(R.layout.wishlist);
+
+            appImageView = (ImageView) findViewById(R.id.pic);
+            appImageView.setImageResource(Global.images[0]);
+
     }
 }
