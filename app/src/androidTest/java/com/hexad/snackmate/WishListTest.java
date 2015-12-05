691fb750557b58d6b7d0fb9d7592d0d0a3c24732
Yilun Liu <yil171@ucsd.edu>
2015-12-04 20:59:23 -0800

    Add more tests

--- app/src/androidTest/java/com/hexad/snackmate/WishListTest.java
+++ app/src/androidTest/java/com/hexad/snackmate/WishListTest.java
@@ -46,7 +46,7 @@ public class WishListTest {
     public ActivityTestRule<HomePageActivity> mActivityRule = new ActivityTestRule<>(HomePageActivity.class);
 
 
-    /* Scenario : add to wish list
+    /* Scenario : add to wish list with an item
         Given a user is on the home page,
         And does not have any wishlist item,
         And home page displays some items,
@@ -64,9 +64,71 @@ public class WishListTest {
         onView(withId(R.id.item_detail_wish)).perform(ViewActions.click());
         // goes to wishlist page
         pressBack();
+//        DrawerActions.openDrawer(R.id.drawer_layout);
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
         onView(withText("Wish List")).perform(ViewActions.click());
         // then there should be one item
+        onData(instanceOf(SnackItem.class)).inAdapterView(withId(R.id.wish_list)).atPosition(0).check(matches(isDisplayed()));
+
+    }
+
+    /* Scenario : add to wish list with same item twice
+        Given a user is on the home page,
+        And does not have any wishlist item,
+        And home page displays some items,
+        When the user clicks a item on the item,
+        And clicks on a add to wish list,
+        And the user clicks the same item again to add to wishlist,
+        And goes to wish list,
+        Then the user sees only one wishlist item
+     */
+    @Test
+    public void AddSameItemtoWishListTwice() {
+
+        // click to go to item detail page
+        onData(anything()).inAdapterView(withId(R.id.homepage_gridview)).atPosition(0).perform(ViewActions.click());
+        // add to wish list twice
+        onView(withId(R.id.item_detail_wish)).perform(ViewActions.click());
+        onView(withId(R.id.item_detail_wish)).perform(ViewActions.click());
+        // goes to wishlist page
+        pressBack();
+//        DrawerActions.openDrawer(R.id.drawer_layout);
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withText("Wish List")).perform(ViewActions.click());
+        // then there should be one item
+        onData(instanceOf(SnackItem.class)).inAdapterView(withId(R.id.wish_list)).atPosition(0).check(matches(isDisplayed()));
+
+    }
+
+    /* Scenario : add to wish list
+        Given a user is on the home page,
+        And does not have any wishlist item,
+        And home page displays some items,
+        When the user clicks a item on the item,
+        And clicks on a add to wish list,
+        And clicks back
+        And the user clicks the another item on homeage to add to wishlist,
+        And goes to wish list,
+        Then the user sees two wishlist item
+    */
+    @Test
+    public void AddTwoDistinctItemsToWishList() {
+
+        // click to go to item detail page
+        onData(anything()).inAdapterView(withId(R.id.homepage_gridview)).atPosition(0).perform(ViewActions.click());
+        // add to wish list
+        onView(withId(R.id.item_detail_wish)).perform(ViewActions.click());
+        // goes back to home page
+        pressBack();
+        // selects another item and add to wishlist
+        onData(anything()).inAdapterView(withId(R.id.homepage_gridview)).atPosition(1).perform(ViewActions.click());
+        onView(withId(R.id.item_detail_wish)).perform(ViewActions.click());
+        // goes back to homepage
+        pressBack();
+//        DrawerActions.openDrawer(R.id.drawer_layout);
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withText("Wish List")).perform(ViewActions.click());
+        // then there should be two item
         onData(instanceOf(SnackItem.class)).inAdapterView(withId(R.id.wish_list)).atPosition(0).check(matches(isDisplayed()));
 
     }
